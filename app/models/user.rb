45bf22530d77dfa5679ed8c0838d055e8ed59a6c
chisaco <chisaco0403@gmail.com>
2018-05-13 23:00:41 +0900

    サインアップ時にnameを登録できるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true, length: { maximum: 20 }
 end
